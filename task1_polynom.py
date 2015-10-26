feb4d0f4dfaf4ce256754f5ba6df7b2ab5697658
Verzhbitski Vladislav <vladislav9526@gmail.com>
2015-10-27 01:12:42 +0300

    Added string parser Fixed Member parsing Changed program philosophy

--- task1_polynom.py
+++ task1_polynom.py
@@ -5,6 +5,10 @@ __author__ = 'Verzhbitski Vladislav'
 
 var = 'x'
 
+src = raw_input()
+var = re.search('[a-z]', src)
+var = var.group(0)
+
 
 def _print(*out):
     for i in out:
@@ -35,7 +39,7 @@ class Polynomial:
 
 class Member:
 
-    def __init__(self, coefx=1, varx='', degreex='0'):
+    def __init__(self, coefx=1, varx='', degreex=0):
         self.coef = coefx
         self.var = varx
         self.degree = degreex
@@ -51,7 +55,10 @@ class Member:
             else:
                 _print('- ')
             sf[0] = 0
-        _print(abs(self.coef))
+
+        if not ((abs(self.coef) == 1) and (self.degree != 0)):
+            _print(abs(self.coef))
+
         if self.degree != 0:
             _print(self.var)
             if self.degree != 1:
@@ -120,64 +127,155 @@ def read_sign(x):
 
 
 def parse_polynomial(src):
-    src = re.sub('([\+\-])\s+', '\g<1>', src)
+    # src = re.sub('([\+\-])\s+', '\g<1>', src)
     # print(src)
     result = Polynomial()
-    member_list = re.split('\s+', src)
-    # tmp_var = re.search('[a-z]', src)
-    # tmp_var = tmp_var.group(0)
-    for i in member_list:
-        tmp = re.findall('([-\+])?(?:([\d]+)(?:([a-z])(?:(?:(?:\^))([\d]+))?)|([\d]+))', i)
-        # print(tmp)
+    # member_list = re.split('\s+', src)
+    # for i in member_list:
+    tmp = re.findall('(?:([\d]*)(?:([a-z])(?:(?:(?:\^))([\d]+))?)|([\d]+))', src)
 
-        if tmp[0][0] == '-':
-            tmp_sign = -1
-        else:
-            tmp_sign = 1
+    # if tmp[0][0] == '-':
+    #     tmp_sign = -1
+    # else:
+    #     tmp_sign = 1
 
-        if tmp[0][4] != '':
-            tmp_degree = 0
-            tmp_coef = tmp_sign * int(tmp[0][4])
+    if tmp[0][3] != '':
+        tmp_degree = 0
+        tmp_coef = int(tmp[0][3])
+    else:
+        if tmp[0][0] != '':
+            tmp_coef = int(tmp[0][0])
         else:
-            tmp_coef = tmp_sign * int(tmp[0][1])
-            if tmp[0][3] == '':
+            tmp_coef = 1
+        if tmp[0][2] == '':
                 tmp_degree = 1
-            else:
-                tmp_degree = int(tmp[0][3])
+        else:
+            tmp_degree = int(tmp[0][2])
 
-        tmp_member = Member(tmp_coef, var, tmp_degree)
-        result.add_member(tmp_member)
+    tmp_member = Member(tmp_coef, var, tmp_degree)
+    result.add_member(tmp_member)
     return result
 
 
-src = raw_input()
-pol = parse_polynomial(src)
-pol.print_polynomial()
-print()
-src2 = raw_input()
-pol1 = parse_polynomial(src2)
-pol1.print_polynomial()
-print()
+def normalize(src):
+    src = re.sub('\s+', '', src)
+    src = re.sub('\)\(', ')*(', src)
+    src = re.sub('([\w])\(', '\g<1>*(', src)
+    src = re.sub('\)(\w)', ')*\g<1>', src)
+    return src
 
-pol1 = res_polynomial(pol1, pol)
-pol1.print_polynomial()
 
-# first = Member(5, 'x', 6)
-# second = Member(-11, 'x', 6)
-#
-# pol = Polynomial()
-# pol.add_member(first)
-# pol.add_member(second)
-# pol.print_polynomial()
-# print()
-# first = Member(-12, 'x', 7)
-# second = Member(17, 'x', 5)
-# pol1 = Polynomial()
-# pol1.add_member(first)
-# pol1.add_member(second)
-# pol1.print_polynomial()
-# print()
-# pol = res_polynomial(pol, pol1)
-# # pol.print_polynomial()
-# # pol.simplification()
-# pol.print_polynomial()
+class Tree:
+
+    def __init__(self):
+        self.left = 0
+        self.right = 0
+        self.value = ''
+
+    def write(self):
+        if self.left == 0 and self.right == 0:
+            _print(self.value)
+            return
+        else:
+            _print('(')
+            self.left.write()
+            _print(self.value)
+            self.right.write()
+            _print(')')
+
+
+def search_close_bracket(src, start):
+    open_b = 1
+    for i in range(start, len(src)):
+        if src[i] == '(':
+            open_b += 1
+        elif src[i] == ')':
+            open_b -= 1
+
+        if open_b == 0:
+            return i
+
+    return -1
+
+
+def clear(src):
+    if search_close_bracket(src, 1) == len(src) - 1:
+        return clear(src[1: -1])
+    else:
+        return src
+
+
+def is_member(src):
+    for i in src:
+        if i in ('+', '-', '(', '*', ')'):
+            return False
+
+    return True
+
+
+def parse(src):
+    result = Tree()
+    first_operation = False
+    mul_position = 0
+    src = clear(src)
+    # print(src)
+    i = 0
+    if not is_member(src):
+        while i < len(src):
+            if src[i] == '(':
+                i = search_close_bracket(src, i + 1)
+            elif src[i] in ('+', '-'):
+                result.left = parse(src[:i])
+                # print(src[:i])
+                result.right = parse(src[i + 1:])
+                # print(src[i + 1:])
+                result.value = src[i]
+                first_operation = True
+            elif src[i] == '*':
+                mul_position = i
+
+            i += 1
+
+        if not first_operation:
+            result.left = parse(src[0: mul_position])
+            # print(src[0: mul_position])
+            result.right = parse(src[mul_position + 1:])
+            # print(src[mul_position + 1:])
+            result.value = '*'
+    else:
+        # print(src)
+        result.value = src
+
+    return result
+
+
+def compute(tree):
+    if tree.left == 0 and tree.right == 0:
+        tree = parse_polynomial(tree.value)
+        # tree.print_polynomial()
+        # print()
+        return tree
+
+    if tree.value == '*':
+        tree = multiply_polynomial(compute(tree.left), compute(tree.right))
+        # tree.print_polynomial()
+        # print()
+        return tree
+    elif tree.value == '+':
+        tree = sum_polynomial(compute(tree.left), compute(tree.right))
+        # tree.print_polynomial()
+        # print()
+        return tree
+    elif tree.value == '-':
+        tree = res_polynomial(compute(tree.left), compute(tree.right))
+        # tree.print_polynomial()
+        # print()
+        return tree
+
+
+src = normalize(src)
+test = parse(src)
+# test.write()
+test = compute(test)
+test.print_polynomial()
+# print(test.left.value)
